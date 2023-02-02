db99f94d2b5be5a9bb2d48d3050c0029ef2582a4
kypanz <kypanz95@gmail.com>
2023-02-02 00:03:14 -0300

    agregado del batch

--- index.js
+++ index.js
@@ -5,24 +5,35 @@ const fs = require('fs');
 function descargar(){
 
 	try{
-	
+
 		// Toma de datos de un archivo txt
 		const result = fs.readFileSync('./link_para_descargar.txt','utf8');
 
-		result.split('\n').forEach((link) => {
-			console.log(link);
+		result.split('\n').forEach(async (link) => {
+
 			// Validacion del link
 			const isLink = ytdl.validateURL(link);
+
+			// Sacando la parte necesaria
+			link = link.slice(32);
+
 			if(!isLink) {
 				// Guardando links no descargados
 				fs.writeFileSync('./no_se_descargaron.txt',link+'\n', { encode : 'utf8', flag : 'a' });
 				console.log('No se descargaron ciertas musicas, fijate en el archivo "no_se_descargaron.txt" de la carpeta')
 			} else {
 				// Obtencion de datos del link
-				const info = ytdl.getInfo(link);
+				const info = await ytdl.getInfo(link);
 				// Descarga del archivo
-				ytdl.download(link)
-				.pipe(fs.writeFileSync(`./musica/${info.videoDetails.title}`));
+				console.log('[ Descargando ] : ', info.videoDetails.title);
+				const stream = ytdl(link, { filter: 'audioonly' });
+				const nuevoNombre = (info.videoDetails.title).split(" ").join("");
+				const file = fs.createWriteStream(`./musica/${nuevoNombre}.mp3`);
+				stream.pipe(file);
+
+				stream.on('end', () => {
+					console.log('[ Descarga completada. ] ' + info.videoDetails.title);
+				});
 			}
 		});
 
